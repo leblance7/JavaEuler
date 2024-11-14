cd9d93637a60ecddcdcc1a78455d8d7e4262dce7
E10 <leblance7@gmail.com>
2024-11-14 16:32:27 -0600

    Cleaning up more code, no longer needs primeFactors<Double>() ArrayList

--- FinalPrime.java
+++ FinalPrime.java
@@ -1,24 +1,24 @@
 /**
  * Create a while loop that:
- *      1) Iterated through a test number finding factors.
- *          - Add factor to ArrayList factors
+ *      1) Iterated through a test number finding primes.
+ *          - Add factor to ArrayList primes
  *      2) Then divides test number by the most factor.
+ * 
  *          - Reducing the max number for while loop.
- *      3) Rename factors and primeFactors ArrayList to where
- *              to where factors is primes. And program no longer
- *              displays old prime factors to user.
+ *      3) Rename primes and primeFactors ArrayList to where
+ *              to where factors becomes primes. And program no longer
+ *              displays old primeFactors to user.
  * Documentation: 
- *      1) Create a logger class or method that can be turned on/off for debugging.
+ *      1) Create a logger class or method that can be turned on/off 
+ *          for debugging.
  *      
  *      2) Make a second version of logger that allows
- *          users to find just the factors of testNum. 
- *          - Possible using a switch case.s
+ *          users to find just the prime factors of testNum. 
  */
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class FinalPrime {
-
 
     public static void main(String[] args) {
                 
@@ -44,15 +44,9 @@ public class FinalPrime {
         /** User input for finding the largest prime */
         System.out.print("Enter the number for testings, heheheh...: ");
         double testNum = input.nextDouble();        
-        double oriNum = testNum;
     
-        /**
-         * ArrayList for factors and primeFactors
-        */
-        ArrayList<Double> factors = new ArrayList<>();
-        ArrayList<Double> primeFactors = new ArrayList<>();
-        primeFactors.add(2.0);
-        
+        /** ArrayList for factors and primeFactors */
+        ArrayList<Double> primes = new ArrayList<>();
         double i = 2.0; // Index value expression
         int l = 0; // Loop index variable
 
@@ -61,26 +55,21 @@ public class FinalPrime {
             E10Logger.LOG("testNum : " + testNum  +  " / " + i + " == " + 
                 testNum % i, debug);
             if(testNum % i == 0) {
-                factors.add(i);
-                E10Logger.LOG("factors: " + factors.get(l), debug);
+                primes.add(i);
+                E10Logger.LOG("primes: " + primes.get(l), debug);
                 E10Logger.LOG("testNum: " + testNum + " / factor: " + 
-                     factors.get(l) + " == " + testNum % factors.get(l) 
+                     primes.get(l) + " == " + testNum % primes.get(l) 
                     , debug);
                 
                 /** testNum divided by factor. */
-                testNum = testNum / factors.get(l);
+                testNum = testNum / primes.get(l);
                 E10Logger.LOG("New testNum: " + testNum, debug);
-            l += 1; /** Goes at the end of the loop, factors index val */ 
+            l += 1; /** Goes at the end of the loop, prime index val */ 
             }
-        i += 1.0; /** Outer loop iterater variable, factors index expression value */
+        i += 1.0; /** Outer loop iterater variable, prime index expression value */
         } /** Outer while loop end */
 
-        if(oriNum % 2 != 0) {
-            primeFactors.remove(0);
-        }
-
-        System.out.println("factors: " + factors);
-        System.out.println("primeFactors: " + primeFactors);
+        System.out.println("primes: " + primes);
         } /** main Method */
 
     } // FinalPrime.java Class file
